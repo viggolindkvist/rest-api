cb8acc5769c4facbf89d0afb46767c09958933ac
Viggo Lindkvist <90836898+viggolindkvist@users.noreply.github.com>
2023-04-24 11:05:03 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -12,6 +12,25 @@ var con = mysql.createConnection({
   database: "restapi",
 });
 
+//funktion för att autentisera en användare
+//Så länge man är inloggad kan man även ändra andra users 
+function authorization(req, res) {
+  let authHeader = req.headers["authorization"];
+  if (authHeader == undefined) {
+    res.status(418).send("auth-header is missing");
+    return false
+  }
+  let token = authHeader.slice(7);
+  let decoded;
+  try {
+    decoded = jwt.verify(token, "signeradochklar");
+  } catch (err) {
+    console.log(err);
+    res.status(401).send("Invalid auth token");
+    return decoded 
+  }
+}
+
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -57,8 +76,10 @@ app.post("/login", function (req, res) {
         name: req.body.username,
       };
       let token = jwt.sign(payload, "signeradochklar");
-      //res.json(answer);
-      res.json(token);
+      res.json({
+        answer: answer,
+        token: token,
+      });
       return;
     } else {
       res.status(418).send("unauthorized");
@@ -86,9 +107,10 @@ app.get("/users", function (req, res) {
 
 //Update User
 app.put("/users/:id", function (req, res) {
-  let passwordhash = hash(req.body.password);
+  let userInfo = authorization(req, res);
+  if (userInfo != false) {
+    let passwordhash = hash(req.body.password);
   let sql = `UPDATE users SET username = '${req.body.username}', password = '${passwordhash}', age = '${req.body.age}' WHERE id = '${req.params.id}'`;
-
   con.query(sql, function (err, result, fields) {
     if (err) throw err;
     let updatedUser = {
@@ -99,6 +121,7 @@ app.put("/users/:id", function (req, res) {
     };
     res.json(updatedUser);
   });
+  }
 });
 
 /*Update User, försök 1 att kolla att id existerar 
